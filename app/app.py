5e83299a810f2998b4fdb46daa07f270ea140168
Telmo Maciel <telmomaciel9@gmail.com>
2024-06-14 15:32:06 +0100

    siuu/fim???

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect
 import requests
 from SPARQLWrapper import SPARQLWrapper, JSON
 from urllib.parse import unquote
@@ -29,6 +29,7 @@ def mostrar_tipos():
       ?documento rdf:type :Documento ;
                  :tipo ?tipo .
     }
+    ORDER BY ?tipo
 
     """
     
@@ -395,6 +396,168 @@ ORDER BY ?id
     dados = resposta.json()['results']['bindings']
 
     return render_template('filtered_documents.html', dados=dados, criterio_procura=criterio_procura)
+
+
+@app.route('/documentos/edit/<id>', methods=['GET'])
+def edit_documento(id):
+    # Consultas SPARQL para obter o documento, emissores e tipos
+    sparql_query_documento = f"""
+    PREFIX : <http://rpcw.di.uminho.pt/2024/DiarioRepublica/>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    
+    SELECT ?id ?tipo ?emissores ?fonte ?data ?notas ?numero ?numeroDR ?series ?pdflink
+    WHERE {{
+        ?documento rdf:type :Documento ;
+                   :éEmitidoPor ?emissores ;
+                   :tipo ?tipo ;
+                   :data ?data ;
+                   :notas ?notas ;
+                   :fonte ?fonte ;
+                   :numero ?numero ;
+                   :numeroDR ?numeroDR ;
+                   :series ?series ;
+                   :pdflink ?pdflink ;
+                   FILTER(STR(?documento) = "http://rpcw.di.uminho.pt/2024/DiarioRepublica#{id}")
+                   
+        BIND(xsd:integer(STRAFTER(str(?documento), "#")) AS ?id)
+    }}
+    """
+    
+    # Obter emissores e tipos como nas outras rotas
+    sparql_query_emissores = """
+    PREFIX : <http://rpcw.di.uminho.pt/2024/DiarioRepublica/>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+    SELECT DISTINCT ?emissor_nome
+    WHERE {
+      ?emissor :nomeEmissor ?emissor_nome .
+    }
+    ORDER BY ?emissor_nome
+    """
+    
+    sparql_query_tipos = """
+    PREFIX : <http://rpcw.di.uminho.pt/2024/DiarioRepublica/>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+
+    SELECT DISTINCT ?tipo
+    WHERE {
+      ?documento rdf:type :Documento ;
+                 :tipo ?tipo .
+    }
+    """
+    
+    try:
+        # Obter documento
+        resposta_documento = requests.get(graphdb_endpoint, params={'query': sparql_query_documento}, headers={'Accept': 'application/sparql-results+json'})
+        if resposta_documento.status_code == 200:
+            item = resposta_documento.json()['results']['bindings'][0]
+        else:
+            return "Erro ao obter o documento."
+
+        # Obter emissores
+        resposta_emissores = requests.get(graphdb_endpoint, params={'query': sparql_query_emissores}, headers={'Accept': 'application/sparql-results+json'})
+        if resposta_emissores.status_code == 200:
+            emissores = [emissor['emissor_nome']['value'] for emissor in resposta_emissores.json()['results']['bindings']]
+        else:
+            return "Erro ao obter os emissores."
+
+        # Obter tipos
+        resposta_tipos = requests.get(graphdb_endpoint, params={'query': sparql_query_tipos}, headers={'Accept': 'application/sparql-results+json'})
+        if resposta_tipos.status_code == 200:
+            tipos = [tipo['tipo']['value'] for tipo in resposta_tipos.json()['results']['bindings']]
+        else:
+            return "Erro ao obter os tipos."
+        
+    except Exception as e:
+        return f"Erro: {e}"
+
+    return render_template('editar_documento.html', item=item, emissores=emissores, tipos=tipos)
+
+@app.route('/documentos/update/<id>', methods=['POST'])
+def update_documento(id):
+    emissor = request.form['emissor']
+    tipo = request.form['tipo']
+    data = request.form['data']
+    notas = request.form['notas']
+    fonte = request.form['fonte']
+    numero = request.form['numero']
+    numeroDR = request.form['numeroDR']
+    series = request.form['series']
+    pdflink = request.form['pdflink']
+
+    if series == "":
+        series = 1
+    emissor = re.sub(r'\s+', '_', emissor)
+    emissor = re.sub(r'[\n\r]+', '', emissor)
+    emissor = emissor.replace(',','').replace('.','').replace('"', '').replace('(','').replace(')','').replace('º','').replace('ª','').replace('«','').replace('»','').replace("'","").replace('/','_').replace('–','').replace('%', 'Porcento').replace('_¿', '').replace('-_','').replace('°', '').replace('!', '').replace('?', '').replace('+', 'Mais').replace('[', '').replace(']', '').replace('_', '').replace('@', '_arroba_').replace('=', '_igual_a_').replace('´', '_').replace('&', 'E')
+
+    query = f"""
+PREFIX : <http://rpcw.di.uminho.pt/2024/DiarioRepublica/>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+DELETE {{
+<http://rpcw.di.uminho.pt/2024/DiarioRepublica#{id}> ?p ?o .
+}}
+INSERT {{
+<http://rpcw.di.uminho.pt/2024/DiarioRepublica#{id}> rdf:type owl:NamedIndividual ,
+                                                        :Documento ;
+                                                      :éEmitidoPor :{emissor} ;
+                                                      :tipo "{tipo}" ;
+                                                      :data "{data}" ;
+                                                      :notas "{notas}" ;
+                                                      :fonte "{fonte}" ;
+                                                      :numero "{numero}" ;
+                                                      :numeroDR "{numeroDR}" ;
+                                                      :series {series} ;
+                                                      :pdflink "{pdflink}" .
+}}
+WHERE {{
+    <http://rpcw.di.uminho.pt/2024/DiarioRepublica#{id}> ?p ?o .
+}}
+"""
+    try:
+        sparql = SPARQLWrapper(graphdb_endpoint + "/statements")
+        sparql.setMethod('POST')
+        sparql.setQuery(query)
+        sparql.setReturnFormat(JSON)
+        sparql.query().convert()
+        return redirect('/documentos')  # Redireciona para a página de documentos após atualização
+    except Exception as e:
+        return render_template('error.html', error=str(e))
+    
+
+
+@app.route('/documentos/delete/<id>', methods=['POST'])
+def delete_documento(id):
+    # Renderizar a página de confirmação
+    documento = {"id": {"value": id}}  # Aqui, você pode buscar mais detalhes se necessário
+    return render_template('confirmar_delete.html', item=documento)
+
+@app.route('/confirm_delete/<id>', methods=['POST'])
+def confirm_delete_documento(id):
+    query = f"""
+PREFIX : <http://rpcw.di.uminho.pt/2024/DiarioRepublica/>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+DELETE WHERE {{
+    <http://rpcw.di.uminho.pt/2024/DiarioRepublica#{id}> ?p ?o .
+}}
+"""
+    try:
+        sparql = SPARQLWrapper(graphdb_endpoint + "/statements")
+        sparql.setMethod('POST')
+        sparql.setQuery(query)
+        sparql.setReturnFormat(JSON)
+        sparql.query().convert()
+        return redirect('/documentos')  # Redireciona para a página de documentos após eliminação
+    except Exception as e:
+        return render_template('error.html', error=str(e))
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
